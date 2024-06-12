6e2d18380c1ce3d260a701abe351343752e5e936
Kazumi Inada <hello@nandenjin.com>
2024-06-12 17:12:01 +0900

    chore: Upload coverage to CodeCov

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { resolve } from "path";
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
@@ -8,6 +9,13 @@ export default defineConfig({
       entry: resolve(__dirname, "lib/index.ts"),
       name: "bvh",
       fileName: "bvh",
+    },
+  },
+
+  test: {
+    coverage: {
+      provider: "istanbul",
+      reporter: ["json"],
     },
   },
 });
