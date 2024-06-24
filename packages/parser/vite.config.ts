1a757330365fb6e9fa918411da2864830ff4b708
Kazumi Inada <hello@nandenjin.com>
2024-06-24 14:24:41 +0900

    chore: Generate type defs

--- packages/parser/vite.config.ts
+++ packages/parser/vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     target: 'modules',
     lib: {
