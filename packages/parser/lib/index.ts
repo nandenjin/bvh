e14354ef3cd17c4212b59dc9de776b1f28682779
Kazumi Inada <hello@nandenjin.com>
2024-12-06 16:01:22 +0900

    feat: add parse options for local coordinates in `parse` function

--- packages/parser/lib/index.ts
+++ packages/parser/lib/index.ts
@@ -17,9 +17,28 @@ export async function read(
   }
 }
 
-export function parse(str: string): BVH {
+export type ParseOptions = {
+  /** Weather to use character-local coords */
+  useLocalCoords?: boolean
+}
+
+export function parse(str: string, options: ParseOptions = {}): BVH {
+  const { useLocalCoords = false } = options
+
   const lines = str.replace('\r', '').split('\n')
-  return new BVH(new Parser(lines).parse())
+
+  const bvh = new BVH(new Parser(lines).parse())
+
+  if (useLocalCoords) {
+    const root = bvh.root
+    for (let i = 0; i < bvh.numFrames; i++) {
+      root.set(i, 'Xposition', 0)
+      root.set(i, 'Yposition', 0)
+      root.set(i, 'Zposition', 0)
+    }
+  }
+
+  return
 }
 
 export { BVH, Parser }
