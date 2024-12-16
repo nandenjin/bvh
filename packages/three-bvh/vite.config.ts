bf4c50d07720f446e1951126ed743806149f3048
Kazumi Inada <hello@nandenjin.com>
2024-12-16 14:58:19 +0900

    fix: Use rollup-ed d.ts file

--- packages/three-bvh/vite.config.ts
+++ packages/three-bvh/vite.config.ts
@@ -4,35 +4,44 @@ import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 import { alias } from '../../config'
 
-export default defineConfig({
-  plugins: [dts()],
-  root: __dirname,
-  resolve: {
-    alias: {
-      ...alias,
+export default defineConfig(({ mode }) => {
+  const isDev = mode == 'development'
+
+  return {
+    plugins: [
+      dts({
+        rollupTypes: true,
+        pathsToAliases: false, // Disable paths to aliases in dts
+      }),
+    ],
+    root: __dirname,
+    resolve: {
+      alias: {
+        ...(isDev ? alias : {}), // Only use alias in dev mode
+      },
     },
-  },
-  build: {
-    target: 'modules',
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['three'],
-      output: {
-        globals: {
-          three: 'THREE',
+    build: {
+      target: 'modules',
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        formats: ['es'],
+        fileName: 'index',
+      },
+      rollupOptions: {
+        external: ['three'],
+        output: {
+          globals: {
+            three: 'THREE',
+          },
         },
       },
     },
-  },
-  test: {
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['json'],
-      exclude: ['playground'],
+    test: {
+      coverage: {
+        provider: 'istanbul',
+        reporter: ['json'],
+        exclude: ['playground'],
+      },
     },
-  },
+  }
 })
