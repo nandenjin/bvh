d11f83a0d47d56ca46d205f8fd56669029592377
Kazumi Inada <hello@nandenjin.com>
2025-01-17 06:27:11 +0000

    chore: Remove unused import

--- packages/parser/test/index.test.ts
+++ packages/parser/test/index.test.ts
@@ -2,13 +2,11 @@ import * as bvh from '../lib'
 import { BVH } from '../lib/bvh'
 import { BVHNode } from '../lib/bvh_node'
 // import { Parser } from "../lib/parser";
-import express from 'express'
 import { readFileSync } from 'fs'
 import { describe, expect, test } from 'vitest'
 import { resolve as resolvePath } from 'path'
 
 const assetsDir = resolvePath(__dirname, '../../../assets')
-
 
 describe('BVH', function () {
   const str = readFileSync(resolvePath(assetsDir, './A_test.bvh'), 'utf-8')
